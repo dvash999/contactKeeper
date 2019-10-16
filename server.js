2a63095ad93eaaa2c1e34ec0dbc6eed5798381b7
dvash999 <myselfandme@gmail.com>
2019-10-16 22:33:22 +0300

    m

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to Contact Keeper API'}));
+// Connect Database
+connectDB();
+
+// Init MiddleWare
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Keeper API' }));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listen on port ${PORT}...`));
